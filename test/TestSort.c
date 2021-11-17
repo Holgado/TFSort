4bb462e089fae557503b9c9ca78296cbf186d71b
Marcelo Holgado <holgadomarcelo184@gmail.com>
2021-11-16 23:22:09 -0300

    Fisrt commit

---
+++ test/TestSort.c
@@ -0,0 +1,19 @@
+#include "unity.h"
+#include "sort.h"
+#include "unity_fixture.h"
+
+
+TEST_GROUP(SORT);
+
+TEST_SETUP(SORT)
+{
+}
+
+TEST_TEAR_DOWN(SORT)
+{
+}
+// Array menor que 6, Inciar por letra, não pode ter caracteres especiais
+
+TEST(SORT, Test1) {
+
+}
